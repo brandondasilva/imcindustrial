b4b1ca87e376e66b6fb00a2931479583f95eb8e0
Brandon Da Silva <da.silva.brandon@gmail.com>
2016-10-30 00:26:04 -0400

    Updated styles

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 
     <!-- Custom styles for this template -->
     <link href="<?php bloginfo('template_directory'); ?>/assets/css/blog.min.css" rel="stylesheet">
+    <link href="<?php bloginfo('template_directory'); ?>/assets/css/styles.min.css" rel="stylesheet">
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -26,18 +27,26 @@
 
   <body>
 
-    <div class="blog-masthead">
-      <div class="container">
-        <nav class="blog-nav">
-          <a class="blog-nav-item active" href="<?php bloginfo('wpurl');?>">Home</a>
-          <?php wp_list_pages('&title_li='); ?>
-        </nav>
+    <div id="navbar">
+      <div class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+          <nav class="blog-nav">
+            <a class="blog-nav-item" href="<?php bloginfo('wpurl');?>"><img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.png" /></a>
+            <?php wp_list_pages('&title_li='); ?>
+          </nav>
+        </div>
       </div>
     </div>
 
-    <div class="container">
+    <div>
 
-      <div class="blog-header">
-        <h1 class="blog-title"><a href="<?php bloginfo('wpurl');?>"><?php echo get_bloginfo('name'); ?></a></h1>
-        <p class="lead blog-description"><?php echo get_bloginfo('description'); ?></p>
+      <div id="header" style="background-image: url(<?php bloginfo('template_directory'); ?>/assets/images/banner.png); " class="blog-header">
+        <div id="header-image" style="background-image: url(<?php bloginfo('template_directory'); ?>/assets/images/gear.png);" class="container">
+          <div id="header-text">
+            <h1 class="blog-title">
+              <a href="<?php bloginfo('wpurl');?>"><?php echo get_bloginfo('name'); ?></a>
+            </h1>
+            <p class="lead blog-description"><?php echo get_bloginfo('description'); ?></p>
+          </div>
+        </div>
       </div>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
+<div class="container">
 	<div class="row">
 
 		<div class="col-sm-8 blog-main">
@@ -9,18 +10,19 @@
 			     get_template_part( 'content', get_post_format() );
         endwhile;
       ?>
-      <nav>
+      <!-- <nav>
       	<ul class="pager">
       		<li><?php next_posts_link( 'Previous' ); ?></li>
       		<li><?php previous_posts_link( 'Next' ); ?></li>
       	</ul>
-      </nav>
+      </nav> -->
       <?php endif; ?>
 
 		</div> <!-- /.blog-main -->
 
-		<?php get_sidebar(); ?>
+		<?php //get_sidebar(); ?>
 
 	</div> <!-- /.row -->
+</div>
 
 <?php get_footer(); ?>
