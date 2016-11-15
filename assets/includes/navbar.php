b5307a3d75848b41321682ba4e0215655153fc46
Brandon Da Silva <da.silva.brandon@gmail.com>
2016-11-15 16:36:19 -0500

    Fixed spacing of links on navbar - it doesn't shift the other options over when hovering on one of the links Fixed styling of anchor tags (links) Edited spacing on services section for more responsiveness

--- assets/includes/navbar.php
+++ assets/includes/navbar.php
@@ -2,10 +2,20 @@
   <div class="navbar navbar-default navbar-fixed-top">
     <div class="container">
       <nav class="blog-nav">
-        <a class="blog-nav-item" href="<?php bloginfo('wpurl');?>"><img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.png" /></a>
-        <?php wp_list_pages('&title_li='); ?>
-        <a href="#">Portfolio</a>
-        <a href="#">Contact</a>
+        <div>
+          <a class="blog-nav-item" href="<?php bloginfo('wpurl');?>">
+            <img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.png" />
+          </a>
+        </div>
+        <div>
+          <?php wp_list_pages('&title_li='); ?>
+        </div>
+        <div>
+          <a href="#">Portfolio</a>
+        </div>
+        <div>
+          <a href="#">Contact</a>
+        </div>
       </nav>
     </div>
   </div>
