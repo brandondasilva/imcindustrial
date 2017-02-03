c5fe094919273e36cefcadf06f5fd7ae9078c435
Brandon Da Silva <da.silva.brandon@gmail.com>
2017-02-03 00:09:04 -0500

    Able to view individual posts with correct URL and content Made responsive Finished individual post styling

--- dev/js/app/controllers/portfolio/PostContentCtrl.js
+++ dev/js/app/controllers/portfolio/PostContentCtrl.js
@@ -19,9 +19,39 @@
 
    function PostContentCtrl($scope, $http, $stateParams) {
 
-     $http.get('index.php/wp-json/posts?filter[category_name]=portfolio&filter[name]=' + $stateParams.slug).success(function(res) {
+     var path = "index.php/wp-json/posts?filter[category_name]=portfolio&";
+
+     $http.get(path + 'filter[name]=' + $stateParams.slug).success(function(res) {
        $scope.post = res[0];
      });
+
+     $scope.latest = [];
+
+     $http.get(path + 'filter[posts_per_page]=3').success(function(res) {
+
+       for (var i = 0; i < res.length; i++) {
+
+         if (res[i].featured_image === null) {
+           $scope.latest.push({
+             heading: res[i].title,
+             desc:    res[i].excerpt,
+             date:    res[i].date,
+             // image:   null,  TODO NEED A PLACEHOLDER IMAGE
+             link:    res[i].guid,
+             slug:    "portfolio/" + res[i].slug
+           });
+         } else {
+           $scope.latest.push({
+             heading: res[i].title,
+             desc:    res[i].excerpt,
+             date:    res[i].date,
+             image:   res[i].featured_image.attachment_meta.sizes.medium_large.url,
+             link:    res[i].guid,
+             slug:    "portfolio/" + res[i].slug
+           });
+         }
+       }
+     })
    }
 
    return ["$scope", "$http", "$stateParams", PostContentCtrl];
