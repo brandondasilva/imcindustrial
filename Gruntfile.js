31afccdbf5703182a64a222256b05b82a071d467
Brandon Da Silva <brandondasilva@MacBook-Pro.local>
2017-03-15 22:19:33 -0400

    Included backend email service Added READMEs for future documentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
         expand: true,
         cwd: 'dev/js',
         src: ['*.js', '**/*.js'],
-        dest: 'assets/js',
+        dest: 'client/assets/js',
         ext: '.js'
       }
     },
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
           expand: true,
           cwd: 'dev/sass',
           src: ['*.scss'],
-          dest: 'dev/css',
+          dest: 'dev/client/css',
           ext: '.css'
         }]
       }
@@ -32,24 +32,15 @@ module.exports = function(grunt) {
           expand: true,
           cwd: 'dev/css',
           src: ['*.css', '!*.min.css'],
-          dest: 'assets/css',
+          dest: 'client/assets/css',
           ext: '.min.css'
         }]
-      }
-    },
-
-    wiredep: {
-      task: {
-        src: [
-          '/*.php'
-        ]
       }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-wiredep');
 
   grunt.registerTask('default', ['uglify', 'sass', 'cssmin']);
   grunt.registerTask('grunt-sass', ['sass']);
---
+++ server/README.md
@@ -0,0 +1,1 @@
+This is only a reference to what the setup is on back end. It is not actually deployed from this folder. You would need another setup for that.
