8bf18a3ab14ed878d35a3839165ce8036a68ad6c
Brandon Da Silva <da.silva.brandon@gmail.com>
2017-04-17 20:00:44 -0400

    Services section complete

--- dev/js/app/controllers/home/HomeServicesCtrl.js
+++ dev/js/app/controllers/home/HomeServicesCtrl.js
@@ -16,29 +16,46 @@
 
 define([], function() {
 
-  function HomeServicesCtrl($scope, path) {
+  function HomeServicesCtrl($scope, $http, path, apiPath) {
 
-    $scope.services = [
-      {
-        heading: 'Rigging',
-        image: path + '/images/pipes.png',
-      }, {
-        heading: 'Fabrication',
-        image: path + '/images/welder.png',
-      }, {
-        heading: 'Millwrighting',
-        image: path + '/images/workers.png',
-      }, {
-        heading: 'Machining',
-        image: path + '/images/workers.png',
-      }, {
-        heading: 'Design',
-        image: path + '/images/workers.png',
+    $http.get(apiPath + 'posts/?filter[category_name]=services&?filter[posts_per_page]=-1').then(function(res) {
+
+      console.log(res.data)
+      $scope.services = [];
+
+      for (var i = 0; i < res.data.length; i++) {
+
+        $scope.services.push({
+          heading: res.data[i].title,
+          image: res.data[i].featured_image.attachment_meta.sizes.medium.url
+        })
       }
-    ];
+
+    }, function(err) {
+
+    });
+
+    // $scope.services = [
+    //   {
+    //     heading: 'Rigging',
+    //     image: path + '/images/pipes.png',
+    //   }, {
+    //     heading: 'Fabrication',
+    //     image: path + '/images/welder.png',
+    //   }, {
+    //     heading: 'Millwrighting',
+    //     image: path + '/images/workers.png',
+    //   }, {
+    //     heading: 'Machining',
+    //     image: path + '/images/workers.png',
+    //   }, {
+    //     heading: 'Design',
+    //     image: path + '/images/workers.png',
+    //   }
+    // ];
 
   }
 
-  return ["$scope", "path", HomeServicesCtrl];
+  return ["$scope", "$http", "path", "apiPath", HomeServicesCtrl];
 
 });
